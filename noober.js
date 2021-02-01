b6a8bb50c5461ba551534c2aed707e6bb7c445de
Daniel Peacock <dan.peacock@kellogg.northwestern.edu>
2021-01-31 21:34:46 -0600

    Consolidated lots of repeated code into functions

--- noober.js
+++ noober.js
@@ -3,73 +3,119 @@ async function pageLoaded() {
   let json = await response.json()
 
   // writes the returned JSON to the console
-  console.dir(json)
+  // console.dir(json)
   
   // 🔥 start here: write code to loop through the rides
-  let ride = []
   let outputElement = document.querySelector('.ridesContainer')
-
-  function buildLeg(passenger) {
-    // passengerName = `${passenger.passengerDetails.first} ${passenger.passengerDetails.last}`
-    // passengerPhone = `${passenger.passengerDetails.phoneNumber}`
-    // passengerNumberOfPassengers = passenger.numberOfPassengers
-    // passengerPickupAddressLine1 = passenger.pickupLocation.address
-    // passengerPickupAddressLine2 = `${passenger.pickupLocation.city}, ${passenger.pickupLocation.state} ${passenger.pickupLocation.zip}`
-    // passengerDropoffAddressLine1 = passenger.dropoffLocation.address
-    // passengerDropoffAddressLine2 = `${passenger.dropoffLocation.city}, ${passenger.dropoffLocation.state} ${passenger.dropoffLocation.zip}`
+  
+  // printLeg() extracts data for one passed passenger and injects a
+  // "ride box" for that rider and their passengers, if any.
+  function printLeg(passenger) {
+    // Code block borrowed from hw3
+    passengerName = `${passenger.passengerDetails.first} ${passenger.passengerDetails.last}`
+    passengerPhone = `${passenger.passengerDetails.phoneNumber}`
+    passengerNumberOfPassengers = passenger.numberOfPassengers
+    passengerPickupAddressLine1 = passenger.pickupLocation.address
+    passengerPickupAddressLine2 = `${passenger.pickupLocation.city}, ${passenger.pickupLocation.state} ${passenger.pickupLocation.zip}`
+    passengerDropoffAddressLine1 = passenger.dropoffLocation.address
+    passengerDropoffAddressLine2 = `${passenger.dropoffLocation.city}, ${passenger.dropoffLocation.state} ${passenger.dropoffLocation.zip}`
     
-    // HTML injection goes here
+    // Enhancement for grammar
+    if (passengerNumberOfPassengers == 1){
+      numPassengers = '1 passenger'
+    } else {
+      numPassengers = `${passengerNumberOfPassengers} passengers`
+      // console.log(`Multiple passengers for ${passengerName}`)
+    }
+
+    // Inject passenger box with relevant data.
+    outputElement.insertAdjacentHTML('beforeend', ` 
+      <div class="border-4 border-gray-900 p-4 my-4 text-left">
+      <div class="flex">
+        <div class="w-1/2">
+          <h2 class="text-2xl py-1">${passengerName}</h2>
+          <p class="font-bold text-gray-600">${passengerPhone}</p>
+        </div>
+        <div class="w-1/2 text-right">
+          <span class="rounded-xl bg-gray-600 text-white p-2">
+            ${numPassengers}
+          </span>
+        </div>
+      </div>
+      <div class="mt-4 flex">
+        <div class="w-1/2">
+          <div class="text-sm font-bold text-gray-600">PICKUP</div>
+          <p>${passengerPickupAddressLine1}</p>
+          <p>${passengerPickupAddressLine2}</p>
+        </div>
+        <div class="w-1/2">
+          <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+          <p>${passengerDropoffAddressLine1}</p>
+          <p>${passengerDropoffAddressLine2}</p>
+        </div>
+      </div>
+    </div>
+  `)
   }
 
-  for (let i = 0; i < json.length; i++) {
-    ride = json[i]
-    if (ride.length > 1) {
-      // If there are more than 1 riders, it must be a Noober Pool
-      // console.log(`${ride.length} legs - Noober Pool - Ride ${i}`)
+  // printRide() accepts a ride and calls printLeg() for each leg of that ride
+  function printRide(ride) {
+    for (let j = 0; j < ride.length; j++) {
+      let passenger = ride[j]
+      printLeg(passenger)
+    }
+  }
+
+  function printLevelOfService(serviceLevel) {
+    if (serviceLevel == 'Pool') {
       outputElement.insertAdjacentHTML('beforeend', ` 
         <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
           <i class="fas fa-car-side"></i>
           <span>Noober Pool</span>
         </h1>
       `)
-      // Passenger 2 details
-      // let passenger2 = ride[1]
-      // buildLeg(passenger2)
-
-      //Passenger 3 details, if present
-      if (ride.length == 3) {
-        // let passenger3 = ride[2]
-        // buildLeg(passenger3)
-        
-      }
-    } else if (ride[0].purpleRequested == true) {
-      // console.log(`Purple is ${ride[0].purpleRequested} - Noober Purple - Ride ${i}`)
+    } else if (serviceLevel == 'Purple') {
       outputElement.insertAdjacentHTML('beforeend', ` 
-        <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
-          <i class="fas fa-car-side"></i>
-          <span>Noober Purple</span>
-        </h1>
-    `)
-
-
-    } else if (ride[0].numberOfPassengers > 3) {
-      // console.log(`${ride[0].numberOfPassengers} riders - Noober XL - Ride ${i}`)
+      <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+        <i class="fas fa-car-side"></i>
+        <span>Noober Purple</span>
+      </h1>
+      `)
+    } else if (serviceLevel == 'XL') {
       outputElement.insertAdjacentHTML('beforeend', ` 
         <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
           <i class="fas fa-car-side"></i>
           <span>Noober XL</span>
         </h1>
       `)
-
-    } else {
-      // console.log(`Noober X - Ride ${i}`)
+    } else if (serviceLevel = 'X') {
       outputElement.insertAdjacentHTML('beforeend', ` 
         <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
           <i class="fas fa-car-side"></i>
           <span>Noober X</span>
         </h1>
       `)
+    } else {
+      console.log(`Error! Service level not defined for Ride ${ride}`)
     }
+  }
+
+  // Determine level of service before writing anything
+  for (let i = 0; i < json.length; i++) {
+    let ride = json[i]
+    let levelOfService = ''
+    if (ride.length > 1) {
+      levelOfService = 'Pool'
+    } else if (ride[0].purpleRequested == true) {
+      levelOfService = 'Purple'
+    } else if (ride[0].numberOfPassengers > 3) {
+      levelOfService = 'XL'
+    } else {
+      levelOfService = 'X'
+    }
+    // Print appropriate level of service and ride
+    printLevelOfService(levelOfService)
+    printRide(ride)
   }
 }
 
