f40716d1847f62f20fa5183990048f86b49c891b
Daniel Peacock <dan.peacock@kellogg.northwestern.edu>
2021-01-31 20:42:59 -0600

    Working logic for level of service

--- noober.js
+++ noober.js
@@ -7,6 +7,60 @@ async function pageLoaded() {
   
   // 🔥 start here: write code to loop through the rides
   
+  let ride = []
+  let outputElement = document.querySelector('.ridesContainer')
+
+  function buildLeg(passenger) {
+    // passengerName = `${passenger.passengerDetails.first} ${passenger.passengerDetails.last}`
+    // passengerPhone = `${passenger.passengerDetails.phoneNumber}`
+    // passengerNumberOfPassengers = passenger.numberOfPassengers
+    // passengerPickupAddressLine1 = passenger.pickupLocation.address
+    // passengerPickupAddressLine2 = `${passenger.pickupLocation.city}, ${passenger.pickupLocation.state} ${passenger.pickupLocation.zip}`
+    // passengerDropoffAddressLine1 = passenger.dropoffLocation.address
+    // passengerDropoffAddressLine2 = `${passenger.dropoffLocation.city}, ${passenger.dropoffLocation.state} ${passenger.dropoffLocation.zip}`
+    
+    // HTML injection goes here
+  }
+
+  for (let i = 0; i < json.length; i++) {
+    ride = json[i]
+    if (ride.length > 1) {
+      // If there are more than 1 riders, it must be a Noober Pool
+
+      console.log(`${ride.length} legs - Noober Pool`)
+      outputElement.insertAdjacentHTML('beforeend', ` 
+        <div class="text-3xl my-8">Test injection for Pool</div>
+      `)
+
+
+      // Passenger 2 details
+      // let passenger2 = ride[1]
+      // buildLeg(passenger2)
+
+      //Passenger 3 details, if present
+      if (ride.length == 3) {
+        // let passenger3 = ride[2]
+        // buildLeg(passenger3)
+        
+      }
+    } else if (ride[0].purpleRequested == true) {
+      console.log(`Purple is ${ride[0].purpleRequested} - Noober Purple`)
+
+
+    } else if (ride[0].numberOfPassengers > 3) {
+      console.log(`${ride[0].numberOfPassengers} riders - Noober XL`)
+
+
+    } else {
+      console.log('Noober X')
+      
+    //   <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+    //   <i class="fas fa-car-side"></i>
+    //   <span>Noober X</span>
+    // </h1>
+
+    }
+  }
 }
 
 window.addEventListener('DOMContentLoaded', pageLoaded)
